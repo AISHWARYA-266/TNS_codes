05da4fde1168676712e33981f56aa7ac1314d2c6
AISHWARYA-266 <143067606+AISHWARYA-266@users.noreply.github.com>
2023-12-15 12:32:32 +0530

    Add files via upload

---
+++ bigger_number.java
@@ -0,0 +1,23 @@
+public class bigger_number {
+
+	public static void main(String[] args) {
+		int a = 60;
+		int b = 20;
+		int c = 30;
+		if(a>b)
+		{
+			System.out.println(a+" a is  bigger");
+		}
+		else if (a<b && b>c)
+		{
+			System.out.println(b+" b is greater");
+		
+		}
+		else {
+			System.out.println(c+" c is greater");
+		}
+
+
+	}
+}
+
